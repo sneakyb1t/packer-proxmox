d882c00fb90352ed7d51cf1fae40da6f1bbee30b
Samir Ahmed Hassn Salem <slmsr90@gmail.com>
2023-05-16 23:51:12 +0200

    working openscap everywhere exept rhel because without subscription

--- rhel/9.2/openscap.sh
+++ rhel/9.2/openscap.sh
@@ -7,3 +7,5 @@ sudo dnf install openscap-scanner scap-security-guide
 # Execute the remediation using the profile and generate a report
 sudo oscap xccdf eval --profile xccdf_org.ssgproject.content_profile_anssi_bp28_minimal \
 /usr/share/xml/scap/ssg/content/ssg-rhel9-ds.xml
+
+exit 0
