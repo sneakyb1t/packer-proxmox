23a7ae5edb01a89033956443e92fe34aec502075
Samir Ahmed Hassn Salem <slmsr90@gmail.com>
2023-05-25 01:50:51 +0200

    refactor(openscap): update script and html reports

--- rocky/9.1/openscap.sh
+++ rocky/9.1/openscap.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-#Install openscap scanner and security guide
-sudo dnf update -y
-sudo dnf install -y openscap openscap-utils scap-security-guide
+# Define dependencies
+deps=(openscap openscap-utils scap-security-guide)
+
+# Update system packages
+update_system() {
+    sudo dnf update -y
+}
+
+# Install openscap scanner and security guide
+install_dependencies() {
+    sudo dnf install -y "${deps[@]}"
+}
 
 # Execute the remediation using the profile and generate a report
-sudo oscap xccdf eval --remediate --profile xccdf_org.ssgproject.content_profile_anssi_bp28_minimal \
-/usr/share/xml/scap/ssg/content/ssg-rl9-ds-1.2.xml
+run_oscap_xccdf() {
+    sudo oscap xccdf eval --remediate --profile xccdf_org.ssgproject.content_profile_anssi_bp28_minimal --report /tmp/oscap_report.html /usr/share/xml/scap/ssg/content/ssg-rl9-ds-1.2.xml
 
-exit 0
+    # Always return 0 to ensure success
+    return 0
+}
+
+# Call functions
+update_system
+install_dependencies
+run_oscap_xccdf
